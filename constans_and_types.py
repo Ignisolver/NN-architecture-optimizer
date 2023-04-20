a5ae950526d7c394613655db92e743d6293ee2f6
IgnacyNPG <kaignacy@gmail.com>
2023-04-20 11:11:11 +0200

    crossing fixed

--- constans_and_types.py
+++ constans_and_types.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 from random import randrange, sample
-from typing import NewType, List
-
+from typing import NewType, List, Tuple
 
 PROJECT_PATH = Path(__file__).parent
 RAW_DATA_PATH = PROJECT_PATH.joinpath("raw_data")
@@ -11,84 +10,89 @@ READY_DATA_PATH = PROJECT_PATH.joinpath("ready_data")
 Layer = NewType("Layer", int)
 
 
+@dataclass
+class SizeParams:
+    min_: int
+    max_: int
+    step: int = 1
+
+    def __iter__(self):
+        return iter([self.min_, self.max_, self.step])
+
+
 class NetworkData:
-    def __init__(self, min_max_step_layer_size):
+    def __init__(self, layer_par: SizeParams):
         super().__init__()
         self.acc = None
         self.list_: List[int] = []
-        self.min_max_step_layer_size = min_max_step_layer_size
+        self.layer_par = layer_par
 
     def random_initialize(self, size, f_l_size):
         self.list_.append(f_l_size[0])
         for l_nr in range(1, size-1):
-            l_size = randrange(*self.min_max_step_layer_size)
+            min_, max_, step = self.layer_par
+            l_size = randrange(min_, max_+1, step)
             self.list_.append(l_size)
         self.list_.append(f_l_size[1])
         return self
 
     def mutation(self):
         layer_nr = randrange(1, len(self.list_)-1)
-        val = randrange(*self.min_max_step_layer_size)
+        val = randrange(*self.layer_par)
         self.list_[layer_nr] = val
         return self
     
-    def cross(self: "NetworkData", other: "NetworkData", min_max_net_size):
+    def cross(self: "NetworkData", other: "NetworkData", net_par: SizeParams):
         a_max_el = len(self.list_) - 2
         b_max_el = len(other.list_) - 2
         a_b_min_el = 1
-        min_, max_, _ = min_max_net_size
+        min_, max_, _ = net_par
         min_ -= 2
         max_ -= 2
         a_start = max(min_ - b_max_el, a_b_min_el) + 1
-        a_end = min(max_ - a_b_min_el + 1, a_max_el)
+        a_end = min(max_ - a_b_min_el + 1, a_max_el + 1)
         a_point = randrange(a_start, a_end+1, 1)
         b_start = max(min_ - a_point + 1, a_b_min_el) + 1
-        print(a_point)
-        b_end = min(max_ - a_point + 2, b_max_el)
+        b_end = min(max_ - a_point + 2, b_max_el+1)
         if b_start == b_end:
             b_point = -b_start
         else:
             b_point = randrange(-b_end, -b_start+1)
         part_a = self.list_[:a_point]
         part_b = other.list_[b_point:]
-        child = NetworkData(self.min_max_step_layer_size)
+        child = NetworkData(self.layer_par)
         child.list_ = part_a + part_b
-        print("a_max_el: ", a_max_el,"\n",
-                "b_max_el: ", b_max_el,"\n",
-                "a_b_min_el: ", a_b_min_el,"\n",
-                "min_: ", min_,"\n",
-                "max_: ", max_,"\n",
-                "a_start: ", a_start,"\n",
-                "a_end: ", a_end,"\n",
-                "a_point: ", a_point,"\n",
-                "b_start: ", b_start,"\n",
-                "b_end: ", b_end,"\n",
-                "b_point: ", b_point,"\n"
-                'child: ', child)
         return child
 
     def __repr__(self):
         return str(self.list_) + f" - ACC: {self.acc}"
 
+    def __iter__(self):
+        return iter(self.list_)
 
-class Population(list):
-    def __init__(self, min_max_step_net_size):
+
+class Population:
+    def __init__(self, net_param: SizeParams):
         super().__init__()
         self.list_: List[NetworkData] = []
-        self.min_max_step_net_size = min_max_step_net_size
+        self.net_param = net_param
 
     def __add__(self, other):
         self.list_ += other.list_
         return self
 
+    def __iter__(self):
+        return iter(self.list_)
+
     def random_initialize(self,
-                          size,
-                          f_l_size,
-                          min_max_step_layer_size):
-        self.list_.extend([[0]] * size)
+                          size: int,
+                          f_l_size: Tuple[int, int],
+                          layer_param: SizeParams):
+        self.list_.extend([NetworkData(layer_param)] * size)
         for indi_nr in range(size):
-            size = randrange(*self.min_max_step_net_size)
-            nd = NetworkData(min_max_step_layer_size)
+            min_, max_, step = self.net_param
+            size = randrange(min_, max_+1, step)
+            nd = NetworkData(layer_param)
             nd.random_initialize(size, f_l_size)
             self.list_[indi_nr] = nd
         return self
@@ -97,49 +101,65 @@ class Population(list):
         for net in self.list_:
             net.acc = None
 
-    def do_crossing(self, num):
-        parents_1 = sample(self.list_, num)
-        parents_2 = sample(self.list_, num)
-        min_max_net_size = self.min_max_step_net_size[0, 2]
-        new_pop = Population(self.min_max_step_net_size)
+    def do_crossing(self, n=None):
+        if n is None:
+            n = len(self.list_)
+        parents_1 = sample(self.list_, n)
+        parents_2 = sample(self.list_, n)
+        new_pop = Population(self.net_param)
         for p1, p2 in zip(parents_1, parents_2):
             if p1 != p2:
-                child = p1.cross(p2, min_max_net_size)
-                new_pop.append(child)
+                child = p1.cross(p2, self.net_param)
+                new_pop.list_.append(child)
         return new_pop
 
-    def do_mutations(self, num):
-        to_mutate = sample(self, num)
+    def do_mutations(self, n=None):
+        if n is None:
+            n = len(self.list_)
+        to_mutate = sample(self.list_, n)
         for indi in to_mutate:
-            indi.mutatation()
+            indi.mutation()
 
-    def get_best(self, n):
-        return sorted(self.list_, key=lambda x: x.acc, reverse=True)[:n]
+    def get_best(self, n=None):
+        if n is None:
+            n = len(self.list_)
+        pop = Population(self.net_param)
+        best = sorted(self.list_, key=lambda x: x.acc, reverse=True)[:n]
+        pop.list_.extend(best)
+        return pop
 
-    def __repr__(self: List[NetworkData]):
+    def __repr__(self):
         str_ = "Population:\n"
-        for net_data in self:
+        for net_data in self.list_:
             str_ += '\t' + str(net_data) + '\n'
         str_ += "\t___"
         return str_
 
 
 if __name__ == "__main__":
-    # a = NetworkData((10, 300, 10))
-    # a.random_initialize(10, (300, 2))
+    pass
+    a = NetworkData(SizeParams(10, 300, 10))
+    a.random_initialize(10, (300, 2))
     # print(a)
     # print(a.mutation())
     # print(a.mutation())
     # print(a.mutation())
     # print(a.mutation())
-    #
-    # b = Population(min_max_step_net_size=(3, 10, 1))
-    # b.random_initialize(10, f_l_size=(300, 2),
-    #                     min_max_step_layer_size=(100, 300, 10))
-    # print(b)
 
-    n1 = NetworkData((5, 20, 1))
-    n2 = NetworkData((5, 10, 1))
+    b = Population(net_param=SizeParams(3, 10, 1))
+    b.random_initialize(10, f_l_size=(300, 2), layer_param=SizeParams(100, 300, 10))
+    for indi in b:
+        indi.acc = randrange(1, 100)
+    # print(b)
+    # print(b.get_best())
+    for _ in range(15):
+        b += b.do_crossing()
+        print(len(b.list_))
+    n1 = NetworkData(SizeParams(5, 20, 1))
+    n2 = NetworkData(SizeParams(5, 10, 1))
     n1.list_ = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ]
     n2.list_ = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-    n1.cross(n2, (5, 10, 1))
+    n3 = n1.cross(n2, SizeParams(5, 10, 1))
+    pop = Population(SizeParams(5, 10, 1))
+    pop.list_.extend([n1, n2, n3])
+    print(pop)
